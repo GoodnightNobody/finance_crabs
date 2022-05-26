5daf9954b0e5bff64f9613599cfba4bef565f605
Konstantin Burmin <konstantinburmingn@gmail.com>
2022-05-26 20:47:11 +0500

    Create class TransactionIn in sqlalchemy

--- models.py
+++ models.py
@@ -1,18 +1,24 @@
-"""
-data = str(input("Введите транзакцию"))  #   -500 (бензин 92)
+import datetime
+
+from sqlalchemy import Column, create_engine, Integer, String, Float, Boolean, DateTime
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('postgresql+psycopg2://konstantinburmin:holly_1203@localhost/crabs', echo=True)
+Base = declarative_base()
 
 
-def deconstructor_data(data):
-    def which_type(data):
-        if data[0] == '-':
-            transaction_type = 'transaction out'
-        elif data[0] == '+':
-            transaction_type = 'transaction in'
-        else:
-            transaction_type = 'not valid'
-        return transaction_type
-    return which_type(data)
+class TransactionIn(Base):
+    __tablename__ = 'transactions_in'
 
-print(deconstructor_data(data))
-"""
-# Place to peewee models
+    id_transaction = Column(Integer, primary_key=True)
+    product_name = Column(String(250), nullable=False)
+    size = Column(String(1), nullable=False)
+    kilo = Column(Float, nullable=False)
+    price_for_kilo = Column(Float, nullable=False)
+    alive = Column(Boolean, nullable=False)
+    delivery = Column(Boolean, nullable=False)
+    coocked = Column(Boolean, nullable=False)
+    date_time = Column(DateTime, default=datetime.datetime.utcnow(), nullable=False)
+
+
+Base.metadata.create_all(engine)
